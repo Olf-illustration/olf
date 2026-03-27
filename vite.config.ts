00b380dc7e33a63e5ee4a54bcc1d67d63dcc22b1
Olf-illustration <florent.labiausse@gmail.com>
2026-03-27 21:23:56 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,21 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
- base: './', //
+  base: './',
   plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
-
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
+  build: {
+    rollupOptions: {
+      // Cette ligne dit à GitHub : "Si tu vois figma:asset, ne plante pas, ignore-le"
+      external: [/^figma:asset/],
+    },
+  },
 })
