c822c47f4f0d92392463553f06c0989c55c6880b
Olf-illustration <florent.labiausse@gmail.com>
2026-03-27 21:29:33 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
+  base: '/olf/',
   plugins: [
     react(),
     tailwindcss(),
